e9f8beaefeb80aa2db8d424dd7ed474f9f6edb94
Angus Chiu <anguschiu1@users.noreply.github.com>
2025-07-25 01:45:51 +0100

    Yo function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 /// A "Hello, world!" program.
 
+/// A "Hello, world!" program.
+fn say_hello() {
+    println!("Hello, world!");
+}
+
+fn say_goodbye() {
+    println!("Goodbye, world!"); // comments
+}
+
+fn say_hello() {
+    println!("Hello, world!");
+}
+
+fn say_goodbye() {
+    println!("Goodbye, world!"); // comments
+}
 fn main() {
     println!("Hello, Jujutsu!");
 }
@@ -8,4 +24,11 @@ fn main() {
 // making more comments, as discussed.
 fn praise() {
     println!("You're gorgeous!")
+}
+// useless comment
+// useless comment
+// Add a comment
+// Add another comment
+fn yo() {
+    println!("yo");
 }
